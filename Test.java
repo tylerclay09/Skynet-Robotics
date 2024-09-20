775ff6b1d062d5a6d701b7e9fdbe85f3c75bef4c
tylerclay09 <tyler.watts@ucas-edu.net>
2024-09-20 03:08:01 +0000

    Added a class called "Test" that prints out "This is a test" when running the program, and changed the filename

---
+++ Test.java
@@ -0,0 +1,7 @@
+// This is a test
+public class Test {
+    public static void main(String[] args) {
+        // prints to console "This is a test"
+        System.out.println("This is a test");
+    }
+}
